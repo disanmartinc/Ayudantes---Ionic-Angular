c9874f928923be63dd35eb624dda792529ae3cf5
disanmartinc <di.sanmartinc@duocuc.cl>
2025-01-15 01:17:47 -0300

    Mistrabajos funcionando completamente

--- src/app/pages/alumnos/mistrabajos/mistrabajos.component.ts
+++ src/app/pages/alumnos/mistrabajos/mistrabajos.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef,ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router'; // Para redirección
 import { AuthService } from '../../../auth.service'; // Ajusta la ruta correctamente
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { MenuAlumnosComponent } from '../../../components/menu-alumnos/menu-alumnos.component';
 import { MenuController } from '@ionic/angular';
 import { TrabajosService } from '../../../services/trabajos.service';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -13,12 +13,15 @@ import { TrabajosService } from '../../../services/trabajos.service';
   templateUrl: './mistrabajos.component.html',
   styleUrls: ['./mistrabajos.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, MenuAlumnosComponent],
+  imports: [CommonModule, IonicModule,FormsModule,],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class MistrabajosComponent implements OnInit {
   trabajos: any[] = [];
   pageTitle: string = 'Mis Trabajos';
-  constructor(private menuCtrl: MenuController,private trabajosService: TrabajosService) {}
+  filteredTrabajos: any[] = []; // Lista filtrada para mostrar en la vista
+  searchTerm: string = ''; // Término de búsqueda
+  constructor(private menuCtrl: MenuController,private trabajosService: TrabajosService,private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     // Habilita el menú al entrar a esta página
@@ -28,8 +31,20 @@ export class MistrabajosComponent implements OnInit {
     this.trabajosService.trabajos$.subscribe((data) => {
       this.trabajos = data; // Asigna la lista al componente
     });
+    this.filterTrabajos(); // Filtra la lista inicialmente
   }
   ionViewWillLeave() {
     this.menuCtrl.enable(false, 'main-menu'); // Deshabilita el menú al salir
   };
+  filterTrabajos() {
+    const term = this.searchTerm.toLowerCase(); // Convierte el término de búsqueda a minúsculas
+    this.filteredTrabajos = this.trabajos.filter((trabajo) =>
+      trabajo.name.toLowerCase().includes(term) // Filtra por el campo `name`
+    );
+  
+    // Depuración
+    console.log('Término de búsqueda:', term);
+    console.log('Trabajos originales:', this.trabajos);
+    console.log('Trabajos filtrados:', this.filteredTrabajos);
+  }
 }
